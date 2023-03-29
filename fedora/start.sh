0c5e9d5564df0fbaa1b83e7a3de02f4e37cbaced
Tyler Hollingsworth <tyholling@gmail.com>
2023-03-29 10:06:44 -0700

    changed to hostfwd

--- fedora/start.sh
+++ fedora/start.sh
@@ -9,6 +9,6 @@ qemu-system-aarch64 \
 -device scsi-hd,drive=disk,bootindex=0 \
 -m 8192 \
 -machine accel=hvf,highmem=on,type=virt \
--nic vmnet-shared,mac=2:0:0:0:0:6 \
+-nic user,hostfwd=tcp::60622-:22 \
 -smp 8 \
 ;
