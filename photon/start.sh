51c8d6cefc96e57c7503cbfb7213784151d213ce
Tyler Hollingsworth <tyholling@gmail.com>
2023-04-09 07:47:26 -0700

    updated networking

--- photon/start.sh
+++ photon/start.sh
@@ -9,7 +9,6 @@ qemu-system-aarch64 \
 -device scsi-hd,drive=disk,bootindex=0 \
 -m 8192 \
 -machine accel=hvf,highmem=on,type=virt \
--nic user,hostfwd=tcp::61622-:22 \
 -nic vmnet-shared,mac=2:0:0:0:0:10 \
 -smp 8 \
 ;
