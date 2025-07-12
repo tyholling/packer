20777f74bd29e21ca962cf3a55b11e2dcbc597e8
Tyler Hollingsworth <tyholling@gmail.com>
2025-07-12 15:21:44 -0700

    add custom ip ranges

--- script/cluster.sh
+++ script/cluster.sh
@@ -2,9 +2,10 @@
 
 function build_node {
   pushd $1
-  ./build.sh ${@:2}
-  for hostname in ${@:2}; do
-    sudo ./provision.sh $hostname
+  ./build.sh ${@:3}
+  address=$2
+  for hostname in ${@:3}; do
+    sudo ./provision.sh $hostname $((address++))
     ssh $hostname bash -s < kubelet.sh
   done
   popd
@@ -12,14 +13,14 @@ function build_node {
 
 cd ..
 
+# control plane
+build_node centos 10 k0 k1 k2
+
 # zone a
-build_node centos a0 a1
+build_node debian 20 a0 a1 a2
 
 # zone b
-build_node debian b0 b1
+build_node fedora 30 b0 b1 b2
 
 # zone c
-build_node fedora c0 c1
-
-# zone d
-build_node ubuntu d0 d1
+build_node ubuntu 40 c0 c1 c2
--- ubuntu/provision.sh
+++ ubuntu/provision.sh
@@ -1,22 +1,31 @@
-#!/bin/bash
+#!/bin/bash -x
 
-[ -z "$1" ] && printf "usage: $0 hostname\n" && exit
-hostname="$1"
+[ -z "$1" ] && printf "usage: $0 hostname\n" && exit || hostname="$1"
 cd $hostname
 
 ../start.sh &
 until [ -f .macaddress ]; do sleep 1; done
+read mac_address < .macaddress
 
-mac_address=$(cat .macaddress)
 mac_reduced=$(echo $mac_address | perl -pe 's/0(\w)/\1/g')
 until arp -an | grep -q " $mac_reduced "; do sleep 1; done
 
 ip_address=$(arp -an | grep " $mac_reduced " | grep -o -m1 "192.168.64.\d\+")
-printf "%-15s %s # %s\n" $ip_address $hostname $mac_address >> /etc/hosts
-printf "[ubuntu]\n$hostname ansible_user=root\n" > .inventory
+ip_updated="192.168.64.$2"
 
 sudo -u $SUDO_USER sh -c "
+printf \"[ubuntu]\n$hostname ansible_host=$ip_address ansible_user=root\n\" > .inventory
+ansible all -i .inventory -m wait_for_connection --ssh-common-args='-o StrictHostKeyChecking=no'
+ansible-playbook -i .inventory ../../ansible/static.yaml -e ip_address=$ip_updated
+ssh -l root $ip_address reboot
+ssh-keygen -R $ip_address
+
+printf \"[ubuntu]\n$hostname ansible_host=$ip_updated ansible_user=root\n\" > .inventory
 ansible all -i .inventory -m wait_for_connection --ssh-common-args='-o StrictHostKeyChecking=no'
 ansible all -i .inventory -m hostname -a name=$hostname
-ansible-playbook -i .inventory ../../ansible/script.yaml
+ansible-playbook -i .inventory ../../ansible/locale.yaml
+ansible-playbook -i .inventory ../../ansible/update.yaml
+ssh-keygen -R $ip_updated
 "
+arp -d $ip_address
+printf "%-15s %s # %s\n" $ip_updated $hostname $mac_address >> /etc/hosts
