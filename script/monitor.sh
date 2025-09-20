6b16855b79116dabbcca1dae25e572cc6993e025
Tyler Hollingsworth <tyholling@gmail.com>
2025-09-20 13:01:53 -0700

    update monitor script

--- script/monitor.sh
+++ script/monitor.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 watch -ct -n1 '
-cat ~/.ssh/known_hosts | awk "{ print \$1; }" | uniq | xargs && echo
+cat ~/.ssh/known_hosts | awk "{ print \$1 }" | uniq | xargs && echo
 pgrep qemu | xargs && echo
 arp -an | grep 2:0: && echo
-tail -n+5 /etc/hosts && echo
+cat /etc/hosts | grep 02:00:
 '
