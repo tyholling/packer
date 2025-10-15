3af27b372c3db3b99dbc553198efd6862f55145e
Tyler Hollingsworth <tyholling@gmail.com>
2025-10-15 09:06:35 -0700

    remove ssh known hosts from monitor script

--- script/monitor.sh
+++ script/monitor.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-watch -ct -n1 '
-cat ~/.ssh/known_hosts | awk "{ print \$1 }" | uniq | xargs && echo
+watch -t -n1 '
 pgrep qemu | xargs && echo
 arp -an | grep 2:0: && echo
 cat /etc/hosts | grep 02:00:
