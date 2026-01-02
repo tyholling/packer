8757a0ae3b3c091454de06e203d908913fda63a8
Tyler Hollingsworth <tyholling@gmail.com>
2026-01-02 09:23:01 -0800

    add machine args to destroy script

--- script/destroy.sh
+++ script/destroy.sh
@@ -4,25 +4,18 @@ shopt -s nullglob
 
 cd ..
 
-for distro in centos debian fedora ubuntu; do
-  pushd $distro > /dev/null
-  for machine in */; do
-    machine="${machine%/}"
-    [ -f $machine/.delete ] && pushd $machine > /dev/null || {
-      echo "skipped: $distro/$machine"
-      continue
-    }
+for machine in "$@"; do
+  for distro in centos debian fedora ubuntu; do
+    [[ -d $distro/$machine ]] && pushd $distro/$machine > /dev/null || continue
     ssh -q -l root $machine poweroff
     ssh-keygen -R $machine &> /dev/null
-    if [ -f .macaddress ]; then
+    if [[ -f .macaddress ]]; then
       read macaddress < .macaddress
       sudo arp -d $(grep $macaddress /etc/hosts | awk '{ print $1 }') > /dev/null
       sudo sed -i '' "/$macaddress/d" /etc/hosts
     fi
     popd > /dev/null
-    rm -rf $machine
+    rm -rf $distro/$machine
     echo "deleted: $distro/$machine"
   done
-  rm -f $distro.pkr.hcl
-  popd > /dev/null
 done
