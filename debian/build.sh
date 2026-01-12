4bb98f89c830a25c9d020e34e0e22df964aa6cb7
Tyler Hollingsworth <tyholling@gmail.com>
2026-01-11 16:12:06 -0800

    update debian

--- debian/build.sh
+++ debian/build.sh
@@ -5,16 +5,16 @@
 jq -cn '$ARGS.positional' --args $@ \
 | gomplate -d "dirs=stdin:?type=application/json" -f debian.pkr.hcl.tpl -o debian.pkr.hcl
 
-if [ ! -f "debian-13.2.0-arm64-DVD-1.iso.sha256" ]; then
+if [ ! -f "debian-13.3.0-arm64-DVD-1.iso.sha256" ]; then
   axel https://cdimage.debian.org/debian-cd/current/arm64/iso-dvd/SHA256SUMS
   cat SHA256SUMS \
-  | sed -n 's/^\([a-z0-9]\{64\}\)  \(debian-13.2.0-arm64-DVD-1.iso\)$/\1  \2/p' \
-  > debian-13.2.0-arm64-DVD-1.iso.sha256
+  | sed -n 's/^\([a-z0-9]\{64\}\)  \(debian-13.3.0-arm64-DVD-1.iso\)$/\1  \2/p' \
+  > debian-13.3.0-arm64-DVD-1.iso.sha256
   rm SHA256SUMS
 fi
 
-if [ ! -f "debian-13.2.0-arm64-DVD-1.iso" ]; then
-  axel https://cdimage.debian.org/debian-cd/current/arm64/iso-dvd/debian-13.2.0-arm64-DVD-1.iso
+if [ ! -f "debian-13.3.0-arm64-DVD-1.iso" ]; then
+  axel https://cdimage.debian.org/debian-cd/current/arm64/iso-dvd/debian-13.3.0-arm64-DVD-1.iso
 fi
 
 packer build debian.pkr.hcl
