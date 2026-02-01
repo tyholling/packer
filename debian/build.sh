b74af8661513ab7a242eceb9b6d79668008e845d
Tyler Hollingsworth <tyholling@gmail.com>
2026-01-31 16:21:24 -0800

    rename debian iso and checksum

--- debian/build.sh
+++ debian/build.sh
@@ -5,16 +5,21 @@
 jq -cn '$ARGS.positional' --args $@ \
 | gomplate -d "dirs=stdin:?type=application/json" -f debian.pkr.hcl.tpl -o debian.pkr.hcl
 
-if [ ! -f "debian-13.3.0-arm64-DVD-1.iso.sha256" ]; then
+if [ ! -f "debian.iso.sha256" ]; then
   axel https://cdimage.debian.org/debian-cd/current/arm64/iso-dvd/SHA256SUMS
+
   cat SHA256SUMS \
-  | sed -n 's/^\([a-z0-9]\{64\}\)  \(debian-13.3.0-arm64-DVD-1.iso\)$/\1  \2/p' \
-  > debian-13.3.0-arm64-DVD-1.iso.sha256
+  | sed -n 's/^\([a-z0-9]\{64\}\)  debian-13.3.0-arm64-DVD-1.iso$/\1  debian.iso/p' \
+  > debian.iso.sha256
+
   rm SHA256SUMS
 fi
 
-if [ ! -f "debian-13.3.0-arm64-DVD-1.iso" ]; then
-  axel https://cdimage.debian.org/debian-cd/current/arm64/iso-dvd/debian-13.3.0-arm64-DVD-1.iso
+if [ ! -f "debian.iso" ]; then
+  axel -o debian.iso \
+  https://cdimage.debian.org/debian-cd/current/arm64/iso-dvd/debian-13.3.0-arm64-DVD-1.iso
+
+  shasum -c debian.iso.sha256
 fi
 
 packer build debian.pkr.hcl
