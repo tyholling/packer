e76e505ecd2b442d0844d8a45080a0778959a07b
Tyler Hollingsworth <tyholling@gmail.com>
2026-01-31 16:33:53 -0800

    rename fedora iso and checksum

--- fedora/build.sh
+++ fedora/build.sh
@@ -5,16 +5,21 @@
 jq -cn '$ARGS.positional' --args $@ \
 | gomplate -d "dirs=stdin:?type=application/json" -f fedora.pkr.hcl.tpl -o fedora.pkr.hcl
 
-if [ ! -f "Fedora-Server-dvd-aarch64-43-1.6.iso.sha256" ]; then
+if [ ! -f "fedora.iso.sha256" ]; then
   axel https://download.fedoraproject.org/pub/fedora/linux/releases/43/Server/aarch64/iso/Fedora-Server-43-1.6-aarch64-CHECKSUM
+
   cat Fedora-Server-43-1.6-aarch64-CHECKSUM \
-  | sed -n 's/^SHA256 (\(Fedora-Server-dvd-aarch64-43-1.6.iso\)) = \([a-z0-9]\{64\}\)$/\2  \1/p' \
-  > Fedora-Server-dvd-aarch64-43-1.6.iso.sha256
+  | sed -n 's/^SHA256 (Fedora-Server-dvd-aarch64-43-1.6.iso) = \([a-z0-9]\{64\}\)$/\1  fedora.iso/p' \
+  > fedora.iso.sha256
+
   rm Fedora-Server-43-1.6-aarch64-CHECKSUM
 fi
 
-if [ ! -f "Fedora-Server-dvd-aarch64-43-1.6.iso" ]; then
-  axel https://download.fedoraproject.org/pub/fedora/linux/releases/43/Server/aarch64/iso/Fedora-Server-dvd-aarch64-43-1.6.iso
+if [ ! -f "fedora.iso" ]; then
+  axel -o fedora.iso \
+  https://download.fedoraproject.org/pub/fedora/linux/releases/43/Server/aarch64/iso/Fedora-Server-dvd-aarch64-43-1.6.iso
+
+  shasum -c fedora.iso.sha256
 fi
 
 packer build fedora.pkr.hcl
