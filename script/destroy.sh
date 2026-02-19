f1b15de8db7876c46a8f771ba1877cde29ac2800
Tyler Hollingsworth <tyholling@gmail.com>
2026-02-19 10:34:47 -0800

    improve destroy script

--- script/destroy.sh
+++ script/destroy.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-shopt -s nullglob
-
-cd ..
+cd $(dirname $0)/..
 
 for machine in "$@"; do
   for distro in centos debian fedora ubuntu; do
     [[ -d $distro/$machine ]] && pushd $distro/$machine > /dev/null || continue
     ssh -q -l root -o ConnectTimeout=1 $machine poweroff
-    ssh-keygen -R $machine &> /dev/null
     if [[ -f .macaddress ]]; then
       read macaddress < .macaddress
-      sudo arp -d $(grep $macaddress /etc/hosts | awk '{ print $1 }') > /dev/null
+      sudo arp -d $macaddress
       sudo sed -i -e "/$macaddress/d" /etc/hosts
     fi
     popd > /dev/null
     rm -rf $distro/$machine
     echo "deleted: $distro/$machine"
   done
+  ssh-keygen -R $machine &> /dev/null
 done
