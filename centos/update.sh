056928bf340744a368cacde1e9e500c4d44b8822
Tyler Hollingsworth <tyholling@gmail.com>
2026-03-05 17:21:39 -0800

    add update scripts

---
+++ centos/update.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+qemu-system-aarch64 \
+-bios /opt/homebrew/share/qemu/edk2-aarch64-code.fd \
+-boot menu=on,splash-time=0 \
+-cpu host \
+-device virtio-rng-device \
+-device virtio-scsi-device \
+-device scsi-hd,drive=disk \
+-display none \
+-drive file=centos.img,if=none,format=raw,id=disk \
+-m 4096 \
+-machine accel=hvf,highmem=on,type=virt \
+-nic user,hostfwd=tcp::60222-:22 \
+-smp 4 \
+&
+
+until ssh -q -l root -p 60222 localhost true; do sleep 1; done
+
+ssh -l root -p 60222 localhost "
+dnf update -y
+dnf autoremove -y
+poweroff
+"
+
+ssh-keygen -R [localhost]:60222
---
+++ ubuntu/update.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+qemu-system-aarch64 \
+-bios /opt/homebrew/share/qemu/edk2-aarch64-code.fd \
+-boot menu=on,splash-time=0 \
+-cpu host \
+-device virtio-rng-device \
+-device virtio-scsi-device \
+-device scsi-hd,drive=disk \
+-display none \
+-drive file=ubuntu.img,if=none,format=raw,id=disk \
+-m 4096 \
+-machine accel=hvf,highmem=on,type=virt \
+-nic user,hostfwd=tcp::60522-:22 \
+-smp 4 \
+&
+
+until ssh -q -l root -p 60522 localhost true; do sleep 1; done
+
+ssh -l root -p 60522 localhost "
+apt-get update -y
+apt-get upgrade -y
+apt-get autoremove -y
+poweroff
+"
+
+ssh-keygen -R [localhost]:60522
