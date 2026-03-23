35cec18698f36e70ea78c0a15080ffc083b867c0
Tyler Hollingsworth <tyholling@gmail.com>
2026-03-22 18:01:00 -0700

    update debian to 13.4.0

--- debian/build.sh
+++ debian/build.sh
@@ -4,13 +4,13 @@
 
 if [ ! -f "debian.iso.sha256" ]; then
   curl -Lfs https://cdimage.debian.org/debian-cd/current/arm64/iso-dvd/SHA256SUMS \
-  | sed -n 's/^\([a-z0-9]\{64\}\)  debian-13.3.0-arm64-DVD-1.iso$/\1  debian.iso/p' \
+  | sed -n 's/^\([a-z0-9]\{64\}\)  debian-13.4.0-arm64-DVD-1.iso$/\1  debian.iso/p' \
   > debian.iso.sha256
 fi
 
 if [ ! -f "debian.iso" ]; then
   axel -o debian.iso \
-  https://cdimage.debian.org/debian-cd/current/arm64/iso-dvd/debian-13.3.0-arm64-DVD-1.iso
+  https://cdimage.debian.org/debian-cd/current/arm64/iso-dvd/debian-13.4.0-arm64-DVD-1.iso
 
   shasum -c debian.iso.sha256
 fi
