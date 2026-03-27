dda8359afbb5104e23abb09cc7e61357d466988a
Tyler Hollingsworth <tyholling@gmail.com>
2026-03-27 16:05:13 -0700

    update provision script to ping and reboot

--- fedora/provision.sh
+++ fedora/provision.sh
@@ -12,7 +12,7 @@ until [ -f .macaddress ]; do sleep 1; done
 read mac_address < .macaddress
 
 mac_reduced=$(echo $mac_address | perl -pe 's/0(\w)/\1/g')
-until arp -an | grep -q " $mac_reduced "; do sleep 1; done
+until arp -an | grep -q " $mac_reduced "; do sleep 1; ping -c1 192.168.64.255 > /dev/null; done
 
 ip_address=$(arp -an | grep " $mac_reduced " | grep -o -m1 "192.168.64.\d\+")
 printf '%-15s %s # %s\n' $ip_address $hostname $mac_address >> /etc/hosts
@@ -33,7 +33,7 @@ ip_updated="$3"
 sudo -E -u $SUDO_USER sh -c "
 ansible-playbook -i .inventory ../../ansible/static.yaml -e ip_address=$ip_updated
 ansible all -i .inventory -m hostname -a name=$hostname
-ssh -l root $hostname reboot
+ssh -l root $hostname reboot || true
 "
 
 sed -i -e "/$mac_address/s/.\{15\}/$(printf %-15s $ip_updated)/" /etc/hosts
