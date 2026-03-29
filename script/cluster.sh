ec190cbde2783a121be1f2848094a58250083512
Tyler Hollingsworth <tyholling@gmail.com>
2026-03-28 18:36:55 -0700

    add apiserver advertise address to cluster script

--- script/cluster.sh
+++ script/cluster.sh
@@ -2,36 +2,39 @@
 
 cd $(dirname $0)
 
-function build_nodes {
+function build_node {
   pushd ../$1
   ./build.sh
   ./worker.sh
-  address=$2
-  for hostname in ${@:3}; do
-    sudo ./provision.sh $hostname worker.img 192.168.64.$((address++))
-  done
+  sudo ./provision.sh $2 worker.img $3
   popd
 }
 
 function add_control {
-  build_nodes $@
-  control_plane_nodes+=(${@:3})
+  local address=$2
+  for node in ${@:3}; do
+    host="192.168.64.$((address++))"
+    build_node $1 $node $host
+    control_plane_nodes+=($node)
+    control_plane_hosts+=($host)
 
-  for s in ${@:3}; do
-    scp kubeadm/kube-vip.yaml root@$s:/etc/kubernetes/manifests
-    scp kubeadm/registry.yaml root@$s:/etc/kubernetes/manifests
-    ssh -l root $s mkdir -p /opt/kubeadm/patches
-    scp kubeadm/patches/* root@$s:/opt/kubeadm/patches/
+    scp kubeadm/kube-vip.yaml root@$node:/etc/kubernetes/manifests
+    scp kubeadm/registry.yaml root@$node:/etc/kubernetes/manifests
+    ssh -l root $node mkdir -p /opt/kubeadm/patches
+    scp kubeadm/patches/* root@$node:/opt/kubeadm/patches/
   done
 
-  for s in ${@:4}; do
-    ssh -l root $s sed -i -e 's/super-admin/admin/' /etc/kubernetes/manifests/kube-vip.yaml
+  for node in ${@:4}; do
+    ssh -l root $node sed -i -e 's/super-admin/admin/' /etc/kubernetes/manifests/kube-vip.yaml
   done
 }
 
 function add_workers {
-  build_nodes $@
-  worker_nodes+=(${@:3})
+  local address=$2
+  for node in ${@:3}; do
+    build_node $1 $node "192.168.64.$((address++))"
+    worker_nodes+=($node)
+  done
 }
 
 add_control centos 10 k0 k1 k2
@@ -40,8 +43,8 @@ add_workers debian 20 a0 a1 a2
 # add_workers ubuntu 40 c0 c1 c2
 
 ssh -l root ${control_plane_nodes[0]} "
-kubeadm init --patches /opt/kubeadm/patches \
---control-plane-endpoint 192.168.64.64 \
+kubeadm init --control-plane-endpoint 192.168.64.64 \
+--apiserver-advertise-address ${control_plane_hosts[0]} --patches /opt/kubeadm/patches \
 --pod-network-cidr 172.20.0.0/16 --service-cidr 172.24.0.0/16
 "
 
@@ -58,11 +61,11 @@ secret=$(kubectl get secrets -n kube-system -o json | jq -r '
 [ .items[]
 | select(.type == "bootstrap.kubernetes.io/token")
 | select(.data."usage-bootstrap-authentication" | @base64d == "true")]
-| sort_by(.data.expiration | @base64d) | reverse | first | .metadata.name
+| sort_by(.data.expiration | @base64d) | last | .metadata.name
 ')
 
 token=$(kubectl get secret -n kube-system $secret -o json | jq -r '
-( .data."token-id" | @base64d ) + "." + ( .data."token-secret" | @base64d )
+.data | (."token-id" | @base64d) + "." + (."token-secret" | @base64d)
 ')
 
 hash=$(kubectl get configmap -n kube-public cluster-info -o json | jq -r '.data.kubeconfig' \
@@ -72,12 +75,14 @@ hash=$(kubectl get configmap -n kube-public cluster-info -o json | jq -r '.data.
 
 pubkey=$(ssh -l root ${control_plane_nodes[0]} kubeadm init phase upload-certs --upload-certs | tail -n1)
 
-for control_plane_node in ${control_plane_nodes[@]:1}; do
-  ssh -l root $control_plane_node "
-  kubeadm join 192.168.64.64:6443 --control-plane --patches /opt/kubeadm/patches \
-  --token $token --discovery-token-ca-cert-hash sha256:$hash --certificate-key $pubkey
+for i in ${!control_plane_nodes[@]}; do
+  [[ $i -eq 0 ]] && continue
+  ssh -l root ${control_plane_nodes[i]} "
+  kubeadm join 192.168.64.64:6443 --control-plane \
+  --apiserver-advertise-address ${control_plane_hosts[i]} --patches /opt/kubeadm/patches \
+  --certificate-key $pubkey --token $token --discovery-token-ca-cert-hash sha256:$hash
   "
-  kubectl wait --for create node $control_plane_node
+  kubectl wait --for create node ${control_plane_nodes[i]}
 done
 
 for worker_node in ${worker_nodes[@]}; do
