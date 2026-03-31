eff67b60af468fe7871397df7ceeec792bb5b1da
Tyler Hollingsworth <tyholling@gmail.com>
2026-03-31 09:45:58 -0700

    add images to monitor script

--- script/monitor.sh
+++ script/monitor.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 watch -tx -n1 bash -c "
+find * -name '*.img' -exec dirname {} \; | sort -u | xargs; echo
 pgrep qemu | xargs; echo
 pr -mt -w \$COLUMNS <(arp -an | grep -E -o '.* 2:0:\S+') <(grep 02:00: /etc/hosts); echo
 grep -v '#' ~/.ssh/known_hosts | awk '{ print \$1 }' | xargs
