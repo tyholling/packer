d5fc9cdcd6d77a94720228c23375d17d6d04feb9
Tyler Hollingsworth <tyholling@gmail.com>
2026-05-03 06:06:33 -0700

    update ubuntu

--- ubuntu/build.sh
+++ ubuntu/build.sh
@@ -3,14 +3,14 @@
 [ -f "ubuntu.img" ] && exit
 
 if [ ! -f "ubuntu.iso.sha256" ]; then
-  curl -Lfs https://cdimage.ubuntu.com/releases/25.10/release/SHA256SUMS \
-  | sed -n 's/^\([a-z0-9]\{64\}\) \*ubuntu-25.10-live-server-arm64.iso$/\1  ubuntu.iso/p' \
+  curl -Lfs https://cdimage.ubuntu.com/releases/26.04/release/SHA256SUMS \
+  | sed -n 's/^\([a-z0-9]\{64\}\) \*ubuntu-26.04-live-server-arm64.iso$/\1  ubuntu.iso/p' \
   > ubuntu.iso.sha256
 fi
 
 if [ ! -f "ubuntu.iso" ]; then
   axel -o ubuntu.iso \
-  https://cdimage.ubuntu.com/releases/25.10/release/ubuntu-25.10-live-server-arm64.iso
+  https://cdimage.ubuntu.com/releases/26.04/release/ubuntu-26.04-live-server-arm64.iso
 
   shasum -c ubuntu.iso.sha256
 fi
