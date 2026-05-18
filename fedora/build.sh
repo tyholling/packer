e175de71504f804b5de1e7114e4cb6d41f9e4004
Tyler Hollingsworth <tyholling@gmail.com>
2026-05-18 13:39:24 -0700

    improve fedora build script

--- fedora/build.sh
+++ fedora/build.sh
@@ -1,18 +1,17 @@
-#!/bin/bash -e -o pipefail
+#!/bin/bash -e
 
-[ -f "fedora.img" ] && exit
+if [ ! -s "fedora.iso.sha256" ]; then
+  curl -LSfs -o fedora.iso.sha256 \
+  https://dl.fedoraproject.org/pub/fedora/linux/releases/44/Server/aarch64/iso/Fedora-Server-44-1.7-aarch64-CHECKSUM
 
-if [ ! -f "fedora.iso.sha256" ]; then
-  curl -Lfs https://dl.fedoraproject.org/pub/fedora/linux/releases/44/Server/aarch64/iso/Fedora-Server-44-1.7-aarch64-CHECKSUM \
-  | sed -n 's/^SHA256 (Fedora-Server-dvd-aarch64-44-1.7.iso) = \([a-z0-9]\{64\}\)$/\1  fedora.iso/p' \
-  > fedora.iso.sha256
+  sed -n -i '' -e 's/^SHA256 (Fedora-Server-dvd-aarch64-44-1.7.iso) = \([a-z0-9]\{64\}\)$/\1  fedora.iso/p' fedora.iso.sha256
 fi
 
-if [ ! -f "fedora.iso" ]; then
+if [ ! -s "fedora.iso" ]; then
   axel -o fedora.iso \
   https://download.fedoraproject.org/pub/fedora/linux/releases/44/Server/aarch64/iso/Fedora-Server-dvd-aarch64-44-1.7.iso
 
   shasum -c fedora.iso.sha256
 fi
 
-packer build -force fedora.pkr.hcl
+[ -s "fedora.img" ] || packer build -force fedora.pkr.hcl
