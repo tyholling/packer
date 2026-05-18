5d2a774b240f38554314dbc2c0e257de37a252bc
Tyler Hollingsworth <tyholling@gmail.com>
2026-05-18 12:13:16 -0700

    improve centos build script

--- centos/build.sh
+++ centos/build.sh
@@ -1,18 +1,17 @@
-#!/bin/bash -e -o pipefail
+#!/bin/bash -e
 
-[ -f "centos.img" ] && exit
+if [ ! -s "centos.iso.sha256" ]; then
+  curl -LSfs -o centos.iso.sha256 \
+  https://mirror.stream.centos.org/10-stream/BaseOS/aarch64/iso/CentOS-Stream-10-latest-aarch64-dvd1.iso.SHA256SUM
 
-if [ ! -f "centos.iso.sha256" ]; then
-  curl -Lfs https://mirror.stream.centos.org/10-stream/BaseOS/aarch64/iso/CentOS-Stream-10-latest-aarch64-dvd1.iso.SHA256SUM \
-  | sed -n 's/^SHA256 (CentOS-Stream-10-latest-aarch64-dvd1.iso) = \([a-z0-9]\{64\}\)$/\1  centos.iso/p' \
-  > centos.iso.sha256
+  sed -n -i '' -e 's/^SHA256 (CentOS-Stream-10-latest-aarch64-dvd1.iso) = \([a-z0-9]\{64\}\)$/\1  centos.iso/p' centos.iso.sha256
 fi
 
-if [ ! -f "centos.iso" ]; then
+if [ ! -s "centos.iso" ]; then
   axel -o centos.iso \
   https://mirror.stream.centos.org/10-stream/BaseOS/aarch64/iso/CentOS-Stream-10-latest-aarch64-dvd1.iso
 
   shasum -c centos.iso.sha256
 fi
 
-packer build -force centos.pkr.hcl
+[ -s "centos.img" ] || packer build -force centos.pkr.hcl
