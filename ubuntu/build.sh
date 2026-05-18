06b80b497c52d01a521aceafdcaab9aae87dde4c
Tyler Hollingsworth <tyholling@gmail.com>
2026-05-18 13:49:17 -0700

    improve ubuntu build script

--- ubuntu/build.sh
+++ ubuntu/build.sh
@@ -1,18 +1,17 @@
-#!/bin/bash -e -o pipefail
+#!/bin/bash -e
 
-[ -f "ubuntu.img" ] && exit
+if [ ! -s "ubuntu.iso.sha256" ]; then
+  curl -LSfs -o ubuntu.iso.sha256 \
+  https://cdimage.ubuntu.com/releases/26.04/release/SHA256SUMS
 
-if [ ! -f "ubuntu.iso.sha256" ]; then
-  curl -Lfs https://cdimage.ubuntu.com/releases/26.04/release/SHA256SUMS \
-  | sed -n 's/^\([a-z0-9]\{64\}\) \*ubuntu-26.04-live-server-arm64.iso$/\1  ubuntu.iso/p' \
-  > ubuntu.iso.sha256
+  sed -n -i '' -e 's/^\([a-z0-9]\{64\}\) \*ubuntu-26.04-live-server-arm64.iso$/\1  ubuntu.iso/p' ubuntu.iso.sha256
 fi
 
-if [ ! -f "ubuntu.iso" ]; then
+if [ ! -s "ubuntu.iso" ]; then
   axel -o ubuntu.iso \
   https://cdimage.ubuntu.com/releases/26.04/release/ubuntu-26.04-live-server-arm64.iso
 
   shasum -c ubuntu.iso.sha256
 fi
 
-packer build -force ubuntu.pkr.hcl
+[ -s "ubuntu.img" ] || packer build -force ubuntu.pkr.hcl
