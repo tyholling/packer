16a72bd6387b83663091d267c50db826da043c6f
Tyler Hollingsworth <tyholling@gmail.com>
2026-05-18 13:28:30 -0700

    improve debian build script

--- debian/build.sh
+++ debian/build.sh
@@ -1,18 +1,17 @@
-#!/bin/bash -e -o pipefail
+#!/bin/bash -e
 
-[ -f "debian.img" ] && exit
+if [ ! -s "debian.iso.sha256" ]; then
+  curl -LOSfs https://cdimage.debian.org/debian-cd/current/arm64/iso-dvd/SHA256SUMS
+  cp SHA256SUMS debian.iso.sha256
 
-if [ ! -f "debian.iso.sha256" ]; then
-  curl -Lfs https://cdimage.debian.org/debian-cd/current/arm64/iso-dvd/SHA256SUMS \
-  | sed -n 's/^\([a-z0-9]\{64\}\)  debian-13.4.0-arm64-DVD-1.iso$/\1  debian.iso/p' \
-  > debian.iso.sha256
+  sed -n -i '' -e 's/^\([a-z0-9]\{64\}\)  debian-.*-arm64-DVD-1.iso$/\1  debian.iso/p' debian.iso.sha256
 fi
 
 if [ ! -f "debian.iso" ]; then
   axel -o debian.iso \
-  https://cdimage.debian.org/debian-cd/current/arm64/iso-dvd/debian-13.4.0-arm64-DVD-1.iso
+  https://cdimage.debian.org/debian-cd/current/arm64/iso-dvd/$(grep 'DVD-1.iso' SHA256SUMS | awk '{ print $2 }')
 
   shasum -c debian.iso.sha256
 fi
 
-packer build -force debian.pkr.hcl
+[ -s "debian.img" ] || packer build -force debian.pkr.hcl
