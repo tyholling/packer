8458ccb835ba6d1d87171ab3db0dc81063b2b61d
Tyler Hollingsworth <tyholling@gmail.com>
2026-05-24 10:16:35 -0700

    reduce monitor script images output

--- script/monitor.sh
+++ script/monitor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 watch -tx -n1 bash -c "
-find * -name '*.img' -exec dirname {} \; | sort -u | xargs; echo
+find * -name '*.img' -exec dirname {} \; | sort -u | xargs basename | xargs; echo
 pgrep qemu | xargs; echo
 pr -mt -w \$COLUMNS <(arp -an | grep -E -o '\(192.168.64.*\) at 2:0:\S+') <(grep 02:00: /etc/hosts); echo
 grep -v '#' ~/.ssh/known_hosts | awk '{ print \$1 }' | xargs
